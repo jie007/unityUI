bc801fa82195abe795c2c854c926c1bdcce96667
unknown <daixingjie@qq.com>
2018-01-05 01:48:24 +0800

    ProxyMgr

--- Scripts/Core/ProxyMgr.cs
+++ Scripts/Core/ProxyMgr.cs
@@ -1,7 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class ProxyMgr
+public class ProxyMgr : INotifier
 {
+    private List<Proxy> m_Proxys = new List<Proxy>();
 
+    public IProxy RetrieveProxy(string name)
+    {
+        IProxy _proxy = m_Proxys.Find(proxy => proxy.ProxyName.Equals(name));
+        return _proxy;
+    }
+
+    public void SendNotification(INotification notification)
+    {
+        m_Proxys.ForEach(proxy => proxy.SendNotification(notification));
+    }
 }
